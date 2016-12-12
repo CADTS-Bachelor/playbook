5f487d317e0912d26076b0f9a3dac98a433458ba
hijack89 <hijack89@163.com>
2016-12-12 16:42:40 +0800

    count.py

--- grade-2015/LiJiahe/count.py
+++ grade-2015/LiJiahe/count.py
@@ -1,10 +1,10 @@
-# coding=utf-8
 #统计单词个数
 #by 李佳贺
 #2016.12.7
 #输入英文纯文本文件，统计其中单词出现次数，输出首先按次数多少其次按字典顺序排列
 
 import sys
+import re
 if len(sys.argv)>1:
     file_name=sys.argv[1]
 else:
@@ -12,14 +12,9 @@ else:
 file=open(file_name,'r')
 text=file.read()
 file.close()
-words=text.split()
-counts=[]
-while(words!=[]):
-    word=words[0]
-    counts.append([-1*words.count(word),word])
-    for i in range(words.count(word)):
-        words.remove(word)
-counts.sort()
+words={}
+for word in re.findall(r'[a-zA-Z0-9]+',text):
+    words[word]=words.get(word,0)+1
+counts=sorted(words.items(),key=lambda x:(-x[1],x[0]))
 for j in range(len(counts)):
-    print(counts[j][1],-1*counts[j][0])
-
+    print(counts[j][0],counts[j][1])
