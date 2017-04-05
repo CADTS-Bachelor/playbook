cfba0960018f32a2d9e6053201812894c217603b
hijack89 <hijack89@163.com>
2017-04-05 18:19:32 +0800

    vbox.py

---
+++ grade-2015/HouDuan/vbox.py
@@ -0,0 +1,30 @@
+from vboxapi import VirtualBoxManager
+
+class VirtualBox(): #Hypervisor
+    def __init__(self):
+        self.mgr = VirtualBoxManager(None, None)
+        self.vbox = self.mgr.vbox
+
+    def power_on(self,vm):
+	name = vm
+	mach = self.vbox.findMachine(name)
+	self.session = self.mgr.getSessionObject(self.vbox)
+	progress = mach.launchVMProcess(self.session, "gui", "")
+	progress.waitForCompletion(-1)
+    
+    def power_off(self):	
+	self.session.console.powerDown()
+	self.mgr.closeMachineSession(self.session)
+
+    def add_nic(self, vm, index, vlan_name):
+        raise NotImplementedError()
+
+#test
+test=VirtualBox()
+name=raw_input()
+test.power_on(name)
+raw_input()
+test.power_off()
+
+
+
