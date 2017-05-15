243b92853bb6fbdded5d44ad6555fc29dfdab972
hijack89 <hijack89@163.com>
2017-05-15 13:02:43 +0800

    vbox.py

--- grade-2015/HouDuan/vbox.py
+++ grade-2015/HouDuan/vbox.py
@@ -1,6 +1,3 @@
-#power_on & power_off
-#by WangPeng & LiJiahe
-
 from vboxapi import VirtualBoxManager
 
 class VirtualBox(): #Hypervisor
@@ -9,25 +6,44 @@ class VirtualBox(): #Hypervisor
         self.vbox = self.mgr.vbox
 
     def power_on(self,vm):
-	name = vm
-	mach = self.vbox.findMachine(name)
-	self.session = self.mgr.getSessionObject(self.vbox)
-	progress = mach.launchVMProcess(self.session, "gui", "")
-	progress.waitForCompletion(-1)
+        name = vm
+        mach = self.vbox.findMachine(name)
+        self.session = self.mgr.getSessionObject(self.vbox)
+        progress = mach.launchVMProcess(self.session, "gui", "")
+        progress.waitForCompletion(-1)
+
     
-    def power_off(self):	
-	self.session.console.powerDown()
-	self.mgr.closeMachineSession(self.session)
+    def power_off(self):
+        self.session.console.powerDown()
+        self.mgr.closeMachineSession(self.session)
+
+    def clone(self,old,new):
+        old_mach = self.vbox.findMachine(old)
+        new_mach = self.vbox.createMachine("",new,"",old_mach.OSTypeId,"")
+        progress = old_mach.cloneTo(new_mach,3,None)
+        print "Cloning,please wait."
+        progress.waitForCompletion(-1)
+        new_mach.saveSettings()
+        self.vbox.registerMachine(new_mach)
 
     def add_nic(self, vm, index, vlan_name):
         raise NotImplementedError()
 
-#test
-test=VirtualBox()
-name=raw_input()
-test.power_on(name)
-raw_input()
-test.power_off()
+if __name__ == "__main__":
+    test = VirtualBox()
+    name = raw_input("name:")
+    test.power_on(name)
+    raw_input()
+    test.power_off()
+    old_mach = name
+    new_mach = raw_input("new_mach:")
+    group = ""
+    try:
+        test.clone(old_mach,new_mach)
+        print "success!"
+    except Exception,e:
+        print e
+
 
 
 
